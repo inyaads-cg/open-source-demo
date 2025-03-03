6e61bb8d22a6356a5165490091d4ae02d34fdcc2
inyaads-cg <60605236+inyaads-cg@users.noreply.github.com>
2025-03-03 09:20:01 -0500

    chore: update the root api

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, open source contributor!');
+  res.send('Hello, open source contributor!!');
 });
 
 // New endpoint to fetch sample data
